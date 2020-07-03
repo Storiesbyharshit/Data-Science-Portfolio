aa5e4720f2c389c55db7ca72dd8a2f1945d4fe05
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-07-03 17:03:41 +0530

    Add files via upload

---
+++ Reinforcement Learning/Apriori-Market-Basket-Optimisation/apriori.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+# Training Apriori 
+from apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualising the results
+results = list(rules) 
+
+#Top Association
+results[0][0]
+
+#Top 10 Association
+for i in range(0,10):
+    print(results[i][0])
